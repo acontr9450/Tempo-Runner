5500d12f2243ed52e405e6e0608c9a08ab3b66a6
acontr9450 <acontr9450@users.noreply.github.com>
2023-10-01 18:18:53 -0500

    Basic Canvas animation

--- tempoRunner.js
+++ tempoRunner.js
@@ -3,11 +3,16 @@ import runner from './runner.js'
 // Declare and assign variables
 const canvas = document.getElementById("trackCanvas");
 const context = canvas.getContext("2d");
+const run1 = document.getElementById("run1");
+const run2 = document.getElementById("run2");
 let runRate = 10;
-let runnerX = 50;
-let runnerY = canvas.height - 100;
-let myRunner = new runner(10, 10, 30, 40);
-myRunner.toggleStride();
+let isTrackRunning = false;
+const runX = 0;
+const runY = canvas.height - 150;
+const runWidth = 100;
+const runHeight = 120;
+
+let myRunner = new runner(runX, runY, runWidth, runHeight);
 
 //myRunner variables testing
 /*console.log(myRunner.getX);
@@ -39,10 +44,42 @@ startButton.addEventListener("click", () => {
 //Animation for the track runner and obstacles
 function trackLoop(){
     //update track game logic here
+    myRunner.toggleStride();
+    let imageY = myRunner.getY;
+    //handle jumping and sliding
+    document.addEventListener("keydown", (event) => {
+        if (event.code === "ArrowUp" && !isActive){
+            //handle jumping
+            myRunner.jump();
 
+        }
+        else if (event.code === "ArrowDown" && !isActive){
+            //handle sliding
+            myRunner.slide();
+        }
+    });
+    //handle letting go of jumping and sliding
+    document.addEventListener(("keyup"), (event) => {
+        if (event.code === "ArrowUp"){
+            //handle stopping jump
+            myRunner.unJump();
+        }
+        else if (event.code === "ArrowDown"){
+            //handle stopping sliding
+            myRunner.slide();
+        }
+    });
     context.clearRect(0, 0, canvas.width, canvas.height);
 
     //draw player character
+    if(myRunner.isStriding == true){
+        context.drawImage(run1, runX, imageY, runWidth, runHeight);
+    }
+    else{
+        context.drawImage(run2, runX, imageY, runWidth, runHeight);
+    }
+    
+    
     //drawing logic for obstacles
 
     //continue game loop
@@ -50,27 +87,5 @@ function trackLoop(){
 
 }
 
-//handle jumping and sliding
-document.addEventListener("keydown", (event) => {
-    if (event.code === "ArrowUp" && !isActive){
-        //handle jumping
-        //increase the y coords of runner
 
-    }
-    else if (event.code === "ArrowDown" && !isActive){
-        //handle sliding
-        //flip the x and y values of runner size
-    }
-});
 
-//handle letting go of jumping and sliding
-document.addEventListener(("keyup"), (event) => {
-    if (event.code === "ArrowUp"){
-        //handle stopping jump
-        //bring y down back to floor
-    }
-    else if (event.code === "ArrowDown"){
-        //handle stopping sliding
-        //reflip the x and y values of runner
-    }
-});
