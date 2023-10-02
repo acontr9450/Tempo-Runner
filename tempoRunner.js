8f9cf6dc3109b4abcafe0c85eba7224d39e596fa
acontr9450 <acontr9450@users.noreply.github.com>
2023-10-02 18:16:45 -0500

    Final Submission Game

--- tempoRunner.js
+++ tempoRunner.js
@@ -7,13 +7,14 @@ const context = canvas.getContext("2d");
 const run1 = document.getElementById("run1");
 const run2 = document.getElementById("run2");
 const hurdles = document.getElementById("hurdles");
-const track = document.getElementById("track");
 
 //Declare game variables
 let frameTimer = 0;
 const runRate = 20;
 let score = 0;
 let isTrackRunning = false;
+let isHurdle = true;
+let count = 1;
 const runX = 100;
 const hurdleX = canvas.width;
 const runY = canvas.height - 150;
@@ -21,6 +22,7 @@ const runWidth = 100;
 const runHeight = 120;
 let myRunner = new runner(runX, runY, runWidth, runHeight);
 let myHurdle = new hurdle(hurdleX, runY, runWidth, runHeight);
+let airHurdle = new hurdle(hurdleX, runY - 125, runWidth, runHeight)
 
 //Start the track game with the start button
 const startButton = document.getElementById("startButton");
@@ -28,14 +30,13 @@ startButton.addEventListener("click", () => {
     if(!isTrackRunning){
         isTrackRunning = true;
         startButton.disabled = true;
-        drawBackground();
         trackLoop();
     }
 });
 
 //handle jumping and sliding when up and down are pressed
  document.addEventListener("keydown", (event) => {
-    if (event.code === "ArrowUp" && !myRunner.isActive && !(score % 20 === 0)){
+    if (event.code === "ArrowUp" && !myRunner.isActive){
         //handle jumping
         myRunner.jump();
     }
@@ -62,41 +63,38 @@ function trackLoop(){
     //declare variables needed for game
     frameTimer +=1;
     score += 1;
-    let imageY = myRunner.getY;
-    let rand = Math.floor(Math.random() * 10);
 
     //set hurdle speed and start moving it
     hurdle.setSpeed(score);
-    myHurdle.move(hurdle.getHurdleSpeed());
 
-    //if hurdle goes off screen create new hurdle object and hurdle drawing
-    if(myHurdle.getX < -100 && rand < 3){
-        myHurdle = new hurdle(hurdleX, runY, runWidth, runHeight);
+    //if hurdle on ground goes off screen create new hurdle object and hurdle drawing
+    if(isHurdle){
+        myHurdle.move(hurdle.getHurdleSpeed());
+        count += 1;
+        drawGame(myRunner, myHurdle, myRunner.isStriding, isHurdle);
+        if(myHurdle.getX < -100){
+            myHurdle = new hurdle(hurdleX, runY, runWidth, runHeight);
+            count = 0;
+            isHurdle = !isHurdle;
+        }
+        
+        
     }
-
-    //if runner is jumping draw the runner jumping
-    if(myRunner.isActive){
-        imageY = myRunner.getY;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        drawJump(imageY, myRunner.isStriding);
-        drawHurdle(myHurdle.getX);
-    }
-    //else draw one of the striding images for runner
+    //else if hurdle on air goes off screen create new hurdle object and hurdle drawing
     else{
-        if(myRunner.isStriding){
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(run1, runX, imageY, runWidth, runHeight);
-            drawHurdle(myHurdle.getX);
+        airHurdle.move(hurdle.getHurdleSpeed());
+        count +=1;
+        drawGame(myRunner, airHurdle, myRunner.isStriding, isHurdle);
+        if(airHurdle.getX < -100){
+            airHurdle = new hurdle(hurdleX, runY - 125, runWidth, runHeight);
+            count = 0;
+            isHurdle = !isHurdle;
         }
-        else{
-            context.clearRect(0, 0, canvas.width, canvas.height);
-            context.drawImage(run2, runX, imageY, runWidth, runHeight);
-            drawHurdle(myHurdle.getX);
-        }
+        
     }
 
     //if the runner and hurdle come into contact, end the game
-    if(gameCondition(myRunner, myHurdle)){
+    if((gameCondition(myRunner, myHurdle) && isHurdle) || (gameCondition(myRunner, airHurdle) && !isHurdle)){
         alert("Game Over!\nYour Score: " + score);
         return score;
     }
@@ -118,24 +116,31 @@ function frameChange(){
     requestAnimationFrame(trackLoop);
 }
 
-//draw track background
-function drawBackground(){
-    context.drawImage(track,0,0,canvas.width,canvas.height);
-}
-
-//draw runner jumping
-function drawJump(y, stride){
+//draw game objects
+function drawGame(runner, hurdle, stride, isHurdle){
+    context.clearRect(0, 0, canvas.width, canvas.height);
     if(stride){
-        context.drawImage(run1, runX, y, runWidth, runHeight);
+        context.drawImage(run1, runX, runner.getY, runWidth, runHeight);
     }
     else{
-        context.drawImage(run2, runX, y, runWidth, runHeight);
+        context.drawImage(run2, runX, runner.getY, runWidth, runHeight);
     }
+
+    if(isHurdle){
+        drawHurdle(hurdle.getX);
+    }
+    else{
+        drawJumpHurdle(hurdle.getX, hurdle.getY);
+    }
+   
 }
 
-//draw hurdle
+//draw hurdle and change xCoord
 function drawHurdle(xCoord){
     context.drawImage(hurdles,xCoord, runY, runWidth, runHeight);
+}
+function drawJumpHurdle(xCoord, yCoord){
+    context.drawImage(hurdles, xCoord, yCoord, runWidth, runHeight);
 }
 
 //handle collision logic of game
